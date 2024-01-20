3e9ae5f1a5693d3a71f135d99a82994a96644ea4
mackliu <macklun@gmail.com>
2024-01-20 09:35:53 +0800

    取出對應的id資料,將資料在表單中顯示

--- 0106/edit.php
+++ 0106/edit.php
@@ -1,13 +1,22 @@
+<?php 
+$dsn="mysql:host=localhost;charset=utf8;dbname=ticket";
+$pdo=new PDO($dsn ,'root','');
+
+$id=$_GET['id'];
+$row=$pdo->query("select * from records where id='$id'")->fetch(PDO::FETCH_ASSOC);
+
+
+?>
 <h1>編輯資料</h1>
 <form action="?" method="post">
     <div>
         <label for="firstname">姓:</label>
         <!--input標籤中的name屬性必需填寫，$_POST或$_GET中才會有對應的key值-->
-        <input type="text" name="firstname" id="">
+        <input type="text" name="firstname" value="<?=$row['firstname'];?>">
     </div>
     <div>
         <label for="lastname">名:</label>
-        <input type="text" name="lastname" id="">
+        <input type="text" name="lastname" value="<?=$row['lastname'];?>">
     </div>
     <div>
         <label for="ordertime">訂票時間:</label>
