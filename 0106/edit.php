b3a51505e7315e2d649e34a6a7b912dafd0b5f78
mackliu <macklun@gmail.com>
2024-01-20 09:59:25 +0800

    完成資料編輯更新資料表語法

--- 0106/edit.php
+++ 0106/edit.php
@@ -2,8 +2,21 @@
 $dsn="mysql:host=localhost;charset=utf8;dbname=ticket";
 $pdo=new PDO($dsn ,'root','');
 
-$id=$_GET['id'];
-$row=$pdo->query("select * from records where id='$id'")->fetch(PDO::FETCH_ASSOC);
+if(isset($_GET['id'])){
+    $id=$_GET['id'];
+    $row=$pdo->query("select * from records where id='$id'")->fetch(PDO::FETCH_ASSOC);
+}
+
+if(!empty($_POST)){
+    $sql="update records set firstname='{$_POST['firstname']}',
+                             lastname='{$_POST['lastname']}',
+                             ordertime='{$_POST['ordertime']}',
+                             num='{$_POST['num']}',
+                             price='{$_POST['price']}' where id='{$_POST['id']}'";
+   $pdo->exec($sql);
+   header("location:db.php");
+}
+
 
 
 ?>
@@ -20,18 +33,19 @@ $row=$pdo->query("select * from records where id='$id'")->fetch(PDO::FETCH_ASSOC
     </div>
     <div>
         <label for="ordertime">訂票時間:</label>
-        <input type="text" name="ordertime" id="">
+        <input type="text" name="ordertime" value="<?=$row['ordertime'];?>">
     </div>
     <div>
         <label for="num">張數:</label>
-        <input type="text" name="num" id="">
+        <input type="text" name="num" value="<?=$row['num'];?>">
     </div>
     <div>
         <label for="price">總價:</label>
-        <input type="text" name="price" id="">
+        <input type="text" name="price" value="<?=$row['price'];?>">
     </div>
     <!-- div>input:submit+input:reset -->
     <div>
+        <input type="hidden" name="id" value="<?=$_GET['id'];?>">
         <input type="submit" value="確認編輯">
         <input type="reset" value="清空重置">
     </div>
